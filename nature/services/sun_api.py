ad0c8641484cd956333ce1b083b312f66591e514
ChungPlusPlus <jjy051221@gmail.com>
2025-05-11 00:21:30 +0900

    feat: cache for sun_api

--- nature/services/sun_api.py
+++ nature/services/sun_api.py
@@ -1,8 +1,15 @@
 import requests
 from datetime import datetime, timedelta
 
+_cached_base_date = None
+_cached_response = None
 
 def fetch_sun_info(lat: float, lng: float, base_date: str, next_date: str):
+    global _cached_base_date, _cached_response
+    
+    if _cached_base_date == base_date:
+        return _cached_response
+
     url = "https://api.sunrise-sunset.org/json"
     params = {
         'lat': lat,
@@ -25,7 +32,9 @@ def fetch_sun_info(lat: float, lng: float, base_date: str, next_date: str):
     next_date_data = next_date_response.json()
     next_date_results = next_date_data['results']
 
-    return {
+    _cached_response = {
         'sunrise': datetime.fromisoformat(next_date_results['sunrise']).replace(tzinfo=None) + timedelta(hours=9),
         'sunset': datetime.fromisoformat(base_date_results['sunset']).replace(tzinfo=None) + timedelta(hours=9)
     }
+
+    return _cached_response
