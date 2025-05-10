16bfb1eb9f9a8ddbdf60b2e662c281ec51ad7d8a
ZirAjs <zirajs7@gmail.com>
2025-05-10 20:35:41 +0900

    Fix: serializers

--- diary/serializers.py
+++ diary/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 from .models import Diary
+from tasks.models import Task
+from tasks.serializers import TaskSerializer
 
 
 class DiarySerializer(serializers.ModelSerializer):
-    # Write-only: for creating/updating with a list of task IDs
+    # For writing: accept list of task IDs
     tasks = serializers.ListField(child=serializers.IntegerField(), write_only=True)
+
+    # For reading: show actual task data
+    task_details = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Diary
@@ -14,5 +19,10 @@ class DiarySerializer(serializers.ModelSerializer):
             "title",
             "description",
             "focus_time",
-            "tasks",
+            "tasks",  # write-only task IDs
+            "task_details",  # read-only serialized task objects
         ]
+
+    def get_task_details(self, obj):
+        tasks = Task.objects.filter(diary_id=obj.id)
+        return TaskSerializer(tasks, many=True).data
