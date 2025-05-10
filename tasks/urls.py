3a2125c9e204532d929840d22f945957a401d3cc
ZirAjs <zirajs7@gmail.com>
2025-05-10 19:43:05 +0900

    Refracter tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import GenerateTasksView
+from .views import TasksViewSet, GenerateTasksView
 
 router = DefaultRouter()
-router.register(r'tasks', TasksViewSet)
+router.register(r"tasks", TasksViewSet, basename="tasks")
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
     path("create-tasks/", GenerateTasksView.as_view(), name="create-tasks"),
 ]
