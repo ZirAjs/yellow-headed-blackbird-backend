2b4e1cefe6b215f71ababb82581e06eb9867e34d
ZirAjs <zirajs7@gmail.com>
2025-05-11 04:40:55 +0900

    Hotfix: auto start time in tasks

--- tasks/models.py
+++ tasks/models.py
@@ -9,5 +9,5 @@ class Task(models.Model):
     description = models.TextField()
     order = models.IntegerField()
     due_time = models.DateTimeField()
-    start_time = models.DateTimeField()
+    start_time = models.DateTimeField(auto_now_add=True)
     finished_time = models.DateTimeField(null=True, blank=True)
