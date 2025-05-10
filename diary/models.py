42688311efa167f4c1796752dfbd8b2eda2bc904
ZirAjs <zirajs7@gmail.com>
2025-05-10 16:39:02 +0900

    Fix FK

--- diary/models.py
+++ diary/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Diary(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    user_id = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField()
     focus_time = models.JSONField()
