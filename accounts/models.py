71d4701d9cae266e3c05a8a8b5f29230e89de623
ZirAjs <zirajs7@gmail.com>
2025-05-11 05:11:29 +0900

    Add: advanced settings

--- accounts/models.py
+++ accounts/models.py
@@ -14,8 +14,18 @@ class User(AbstractUser):
 
 class Setting(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="setting")
+
     # theme = models.CharField(max_length=20, default="light")
-    alarm = models.BooleanField(default=True)
+    class AlarmTime(models.IntegerChoices):
+        NONE = 0, "no alarm"
+        HALF = 30, "30min"
+        MAXIMUM = 60, "60min"
+
+    alarm = models.IntegerField(
+        choices=AlarmTime,  # hook in the enum
+        default=AlarmTime.NONE,  # default via enum member
+    )
+    alarm_ui = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.user.username}'s settings"
