f95c0ab3fa35d27c56f962fe09b16ae428f04c9f
ZirAjs <zirajs7@gmail.com>
2025-05-10 18:06:46 +0900

    Add functionalities of diary

--- config/urls.py
+++ config/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path(
         "api/",
         include("diary.urls"),
-        name="diary",
+        name="diaries",
     ),  # diary 앱의 urls.py를 포함
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
--- diary/urls.py
+++ diary/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import DiaryViewSet
 
 router = DefaultRouter()
-router.register(r"diaries", DiaryViewSet, basename="diary")
+router.register(r"diaries", DiaryViewSet, basename="diaries")
 urlpatterns = [
     path("", include(router.urls)),
 ]
