a16bb006fde739e811f4a2b915185e13cc9d3596
ZirAjs <zirajs7@gmail.com>
2025-05-11 05:30:21 +0900

    Hotfix: modified enum value to match frontend

--- accounts/models.py
+++ accounts/models.py
@@ -18,8 +18,8 @@ class Setting(models.Model):
     # theme = models.CharField(max_length=20, default="light")
     class AlarmTime(models.IntegerChoices):
         NONE = 0, "no alarm"
-        HALF = 30, "30min"
-        MAXIMUM = 60, "60min"
+        HALF = 1, "30min"
+        MAXIMUM = 2, "60min"
 
     alarm = models.IntegerField(
         choices=AlarmTime,  # hook in the enum
@@ -29,6 +29,7 @@ class Setting(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s settings"
+
 
 class Cutoff(models.Model):
     name = models.CharField(max_length=200)
